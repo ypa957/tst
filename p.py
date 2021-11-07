bd9b7050bd166a96275cb162a69aac08f97f238f
yp <mendrya1@163.com>
2021-11-07 19:17:03 +0800

    show

--- p.py
+++ p.py
@@ -1,7 +1,5 @@
-import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
-
-
-print("hello world!")
-print('New branch!')
+import numpy as np
+fig = plt.figure(num=1, figsize=(4, 4))
+plt.plot([1, 2, 3, 4], [1, 2, 3, 4])
+plt.show()
