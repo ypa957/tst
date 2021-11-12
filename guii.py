8e207c5ed61553e98ddba6f769f50d17e6241794
yp <mendrya1@163.com>
2021-11-12 17:41:55 +0800

    define

--- guii.py
+++ guii.py
@@ -6,6 +6,8 @@ def main():
     parser = GooeyParser(description="第一个示例!")
     parser.add_argument('文件路径', widget="FileChooser")      # 文件选择框
     parser.add_argument('日期', widget="DateChooser")          # 日期选择框
+    parser.add_argument('yp', widget="DateChooser")
+    parser.add_argument('ypa', widget="FileChooser")
     args = parser.parse_args()                                 # 接收界面传递的参数
     print(args)
 
--- p.py
+++ p.py
@@ -3,6 +3,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-fig = plt.figure(num=1, figsize=(4, 4))
-plt.plot([1, 2, 3, 4], [1, 2, 3, 4])
-plt.show()
+def main():
+    fig = plt.figure(num=1, figsize=(4, 4))
+    plt.plot([1, 2, 3, 4], [1, 2, 3, 4])
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
